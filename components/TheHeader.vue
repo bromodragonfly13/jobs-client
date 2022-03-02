d19129c9e5ea82b9826f6d87584b73a1241425a6
Alexandr Shaposhnik <alexandrshaposhnik@Alexandrs-MacBook-Pro.local>
2022-03-02 18:20:48 +0200

    add pages

---
+++ components/TheHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="py-6 flex items-center justify-between">
+        <nuxt-link :to="{name: 'index'}" exact-active-class="text-blue-500">Jobs</nuxt-link>
+        <div class="-mx-4 flex items-center flex-wrap">
+            <span class="mx-4 font-bold">Alex</span>
+            <nuxt-link :to="{name: 'login'}" class="mx-4" exact-active-class="text-blue-500">Login</nuxt-link>
+            <a href="" class="mx-4" exact-active-class="text-blue-500">Manage listings</a>
+            <a href="" class="mx-4" exact-active-class="text-blue-500">Logout</a>
+            <a href="" class="mx-4" exact-active-class="text-blue-500">Post a job</a>
+        </div>
+    </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto px-6">
+    <TheHeader />
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+</style>
