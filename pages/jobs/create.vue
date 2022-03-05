16c6181dd2c2949773d0bffe5c299912f568da67
Alexandr Shaposhnik <alexandrshaposhnik@Alexandrs-MacBook-Pro.local>
2022-03-05 04:20:06 +0200

    validation

--- pages/jobs/create.vue
+++ pages/jobs/create.vue
@@ -4,42 +4,64 @@
             <div class="mb-10">
                 <h2 class="mb-4 text-2xl font-bold">Listing details</h2>
 
-                {{form}}
-                
                 <div class="mb-4">
                     <label for="job_title" class="inline-block mb-1 font-medium">Job title</label>
                     <input type="text" name="job_title" id="job_title" class="bg-gray-200 border-2
-                    border-gray-200 rounded-lg w-full h-10 px-4" v-model="form.job_title">
+                    border-gray-200 rounded-lg w-full h-10 px-4" :class="{'border-red-500': errors['input.job_title']}" v-model="form.job_title">
+
+                    <div class="text-sm text-red-500 mt-1" v-if="errors['input.job_title']">
+                        {{errors['input.job_title'][0]}}
+                    </div>
                 </div>
 
                 <div class="mb-4">
                     <label for="job_location" class="inline-block mb-1 font-medium">Job location</label>
                     <input type="text" name="job_location" id="job_location" class="bg-gray-200 border-2
-                    border-gray-200 rounded-lg w-full h-10 px-4" v-model="form.job_location ">
+                    border-gray-200 rounded-lg w-full h-10 px-4" :class="{'border-red-500': errors['input.job_location']}" v-model="form.job_location ">
+
+                    <div class="text-sm text-red-500 mt-1" v-if="errors['input.job_location']">
+                        {{errors['input.job_location'][0]}}
+                    </div>
                 </div>
 
                 <div class="mb-4">
                     <label for="job_link" class="inline-block mb-1 font-medium">Job application URL</label>
                     <input type="text" name="job_link" id="job_link" class="bg-gray-200 border-2
-                    border-gray-200 rounded-lg w-full h-10 px-4" v-model="form.job_link">
+                    border-gray-200 rounded-lg w-full h-10 px-4" :class="{'border-red-500': errors['input.job_link']}" v-model="form.job_link">
+
+                    <div class="text-sm text-red-500 mt-1" v-if="errors['input.job_link']">
+                        {{errors['input.job_link'][0]}}
+                    </div>
                 </div>
 
                 <div class="mb-4">
                     <label for="tags" class="inline-block mb-1 font-medium">Tags</label>
                     <v-select inputId="tags" label="title" :reduce="tag => tag.id" 
-                        :options="tags" multiple v-model="form.tags"> </v-select>
+                        :options="tags" multiple class="border-2 border-gray-200 rounded-lg" :class="{'border-red-500': errors['input.tags.connect']}" v-model="form.tags"> </v-select>
+
+                    <div class="text-sm text-red-500 mt-1" v-if="errors['input.tags.connect']">
+                            {{errors['input.tags.connect'][0]}}
+                    </div>
                 </div>
 
                 <div class="mb-4">
                     <label for="company_name" class="inline-block mb-1 font-medium">Company name</label>
                     <input type="text" name="company_name" id="company_name" class="bg-gray-200 border-2
-                    border-gray-200 rounded-lg w-full h-10 px-4" v-model="form.company_name">
+                    border-gray-200 rounded-lg w-full h-10 px-4" :class="{'border-red-500': errors['input.company_name']}" v-model="form.company_name">
+
+                    <div class="text-sm text-red-500 mt-1" v-if="errors['input.company_name']">
+                        {{errors['input.company_name'][0]}}
+                    </div>
                 </div>
 
                 <div class="mb-4">
                     <label for="company_logo" class="inline-block mb-1 font-medium">Company logo URL</label>
                     <input type="text" name="company_logo" id="company_logo" class="bg-gray-200 border-2
-                    border-gray-200 rounded-lg w-full h-10 px-4" v-model="form.company_logo">
+                    border-gray-200 rounded-lg w-full h-10 px-4" :class="{'border-red-500': errors['input.company_logo']}" v-model="form.company_logo">
+
+                    <div class="text-sm text-red-500 mt-1" v-if="errors['input.company_logo']">
+                        {{errors['input.company_logo'][0]}}
+                    </div>
                 </div>
 
                 <div class="mb-4">
@@ -61,19 +83,31 @@
                     <div class="mb-4">
                         <label for="email" class="inline-block mb-1 font-medium">Email</label>
                         <input type="email" name="email" id="email" class="bg-gray-200 border-2
-                        border-gray-200 rounded-lg w-full h-10 px-4" v-model="form.user_email">
+                        border-gray-200 rounded-lg w-full h-10 px-4" :class="{'border-red-500': errors['input.user.create.email']}" v-model="form.user_email">
+
+                        <div class="text-sm text-red-500 mt-1" v-if="errors['input.user.create.email']">
+                            {{errors['input.user.create.email'][0]}}
+                        </div>
                     </div>
 
                     <div class="mb-4">
                         <label for="email" class="inline-block mb-1 font-medium">Name</label>
                         <input type="text" name="name" id="name" class="bg-gray-200 border-2
-                        border-gray-200 rounded-lg w-full h-10 px-4" v-model="form.user_name">
+                        border-gray-200 rounded-lg w-full h-10 px-4" :class="{'border-red-500': errors['input.user.create.name']}" v-model="form.user_name">
+
+                        <div class="text-sm text-red-500 mt-1" v-if="errors['input.user.create.name']">
+                            {{errors['input.user.create.name'][0]}}
+                        </div>
                     </div>
 
                     <div class="mb-4">
                         <label for="password" class="inline-block mb-1 font-medium">Password</label>
                         <input type="password" name="name" id="password" class="bg-gray-200 border-2
-                        border-gray-200 rounded-lg w-full h-10 px-4" v-model="form.user_password">
+                        border-gray-200 rounded-lg w-full h-10 px-4" :class="{'border-red-500': errors['input.user.create.password']}" v-model="form.user_password">
+
+                        <div class="text-sm text-red-500 mt-1" v-if="errors['input.user.create.password']">
+                            {{errors['input.user.create.password'][0]}}
+                        </div>
                     </div>
 
                 </div>
@@ -93,6 +127,7 @@
     export default{
         data() {
             return{
+                errors: {},
                 form:{
                     job_title: '',
                     job_location: '',
@@ -132,6 +167,9 @@
                         this.$router.replace({ name: 'index'})
                     })
                 })
+            }).catch((e) => {
+                this.errors = e.graphQLErrors[0].extensions.validation
+
             })
             },
             submit(){
